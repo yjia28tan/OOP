acacda0298d29906d1ddecc0fe9d82a9ff2e6450
YiJia 钰嘉 <99154734+yjia28tan@users.noreply.github.com>
2022-09-28 20:51:47 +0800

    Update main.cpp

--- Rectangle1/main.cpp
+++ Rectangle1/main.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
 	Rectangle box;    // Define an instance of the Rectangle class
 					  // box is object of Rectangle
    double rectWidth;  // Local variable for width
